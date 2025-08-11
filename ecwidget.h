61ebd28bba4404901c77dcc332f395833865cd6a
Dena Achmad <denaachmad.ab@gmail.com>
2025-08-11 13:24:35 +0700

    Enhance create route function with waypoint and legline projection, and add contect menu when right click on chart and waypoint

--- ecwidget.h
+++ ecwidget.h
@@ -10,6 +10,8 @@
 #include <QMutex>
 #include <QMutexLocker>
 #include <QTextEdit>
+#include <QMenu>
+#include <QAction>
 
 // SevenCs Kernel EC2007
 #ifdef _WIN32
@@ -252,6 +254,11 @@ public:
   void removeWaypointAt(int x, int y);
   void moveWaypointAt(int x, int y);
   void editWaypointAt(int x, int y);
+  
+  // Context menu functions
+  int findWaypointAt(int x, int y);
+  void showWaypointContextMenu(const QPoint& pos, int waypointIndex);
+  void showMapContextMenu(const QPoint& pos);
   bool resetWaypointCell();
   void drawLeglineLabels();
   void drawRouteLines(); // Gambar garis route dengan warna berbeda per route (DEPRECATED)
