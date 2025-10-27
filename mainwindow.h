c29f5031d55693a910af55722beb2471db8dacf5
Ali Hasan Ash Shiddiq <72016395+alihasanash@users.noreply.github.com>
2025-10-27 10:15:21 +0700

    ECDIS v1.43 Stable

--- mainwindow.h
+++ mainwindow.h
@@ -39,7 +39,7 @@ class QFile;
 // data server id
 
 #define DSID "0"
-#define APP_TITLE "ECDIS AUV v1.4"
+#define APP_TITLE "ECDIS AUV v1.43"
 
 #ifndef DWMWA_USE_IMMERSIVE_DARK_MODE
 #define DWMWA_USE_IMMERSIVE_DARK_MODE 20
